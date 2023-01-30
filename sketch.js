faceb4c2684d6388d608c9f721170727c88707bc
arti-garg <75868830+arti-garg@users.noreply.github.com>
2023-01-31 05:02:38 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+
+
+function preload() {
+  backgroundImage = loadImage("./assets/bg.png");
+  
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  
+}
+
+function draw() {
+  background(backgroundImage);
+  
+}
